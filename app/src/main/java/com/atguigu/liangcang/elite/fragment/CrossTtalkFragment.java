d383b0a5c02e3e6d2a34d2fafe07be1367e0d65d
maxiaowen <332168460@qq.com>
2017-07-11 11:01:56 +0800

    段子页面完成

--- app/src/main/java/com/atguigu/liangcang/elite/fragment/CrossTtalkFragment.java
+++ app/src/main/java/com/atguigu/liangcang/elite/fragment/CrossTtalkFragment.java
@@ -1,11 +1,27 @@
 package com.atguigu.liangcang.elite.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.atguigu.liangcang.R;
 import com.atguigu.liangcang.base.BaseFragment;
+import com.atguigu.liangcang.elite.adapter.RecommendAdapter;
+import com.atguigu.liangcang.elite.bean.RecommendBean;
+import com.atguigu.liangcang.utils.UIUtils;
+import com.cjj.MaterialRefreshLayout;
+import com.cjj.MaterialRefreshListener;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+
 
 /**
  * Created by Administrator on 2017/7/10.
@@ -13,20 +29,111 @@ import com.atguigu.liangcang.base.BaseFragment;
 
 public class CrossTtalkFragment extends BaseFragment {
 
-    TextView textView;
+
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @Bind(R.id.refresh)
+    MaterialRefreshLayout refresh;
+    @Bind(R.id.progressbar)
+    ProgressBar progressbar;
+    @Bind(R.id.tv_nomedia)
+    TextView tvNomedia;
+
+    private String url = "http://s.budejie.com/topic/tag-topic/64/hot/budejie-android-6.6.3/0-20.json";
+
+    private RecommendAdapter adapter;
 
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(30);
-        textView.setTextColor(Color.RED);
-        textView.setGravity(Gravity.CENTER);
-        return textView;
+        View view = View.inflate(context, R.layout.fragment_cross, null);
+        ButterKnife.bind(this, view);
+
+        refresh.setMaterialRefreshListener(new MaterialRefreshListener() {
+            //下拉刷新
+            @Override
+            public void onRefresh(MaterialRefreshLayout materialRefreshLayout) {
+                netWorking();
+            }
+
+            //            //加载更多-上拉刷新   不做啦
+            @Override
+            public void onRefreshLoadMore(MaterialRefreshLayout materialRefreshLayout) {
+                super.onRefreshLoadMore(materialRefreshLayout);
+
+                //结束上拉刷新
+                refresh.finishRefreshLoadMore();
+            }
+        });
+
+        return view;
     }
 
     @Override
     public void initData() {
+        //连网请求数据
+        netWorking();
 
-        textView.setText("段子");
+    }
+
+    private void netWorking() {
+        //开启分线程
+        UIUtils.getGlobalThread().execute(new Runnable() {
+            @Override
+            public void run() {
+                //连网
+                OkHttpUtils
+                        .get()
+                        .url(url)
+                        .build()
+                        .execute(new StringCallback() {
+                            @Override
+                            public void onError(Call call, Exception e, int id) {
+                                Log.e("TAG", "联网失败==" + e.getMessage());
+                            }
+
+                            @Override
+                            public void onResponse(String response, int id) {
+                                Log.e("TAG", "RecommendFragment联网成功==" + response);
+                                //下拉刷新结束
+                                refresh.finishRefresh();
+                                //解析数据
+                                parseData(response);
+                            }
+                        });
+            }
+        });
+    }
+
+    //解析数据
+    private void parseData(String json) {
+
+        RecommendBean recommendBean = new Gson().fromJson(json, RecommendBean.class);
+
+        Log.e("TAG", "name ==" + recommendBean.getList().get(0).getTags().get(0).getName());
+
+        if (recommendBean.getList() != null && recommendBean.getList().size() > 0) {
+            //有视频
+            tvNomedia.setVisibility(View.GONE);
+            //设置适配器
+            adapter = new RecommendAdapter(context, recommendBean.getList());
+            recyclerView.setAdapter(adapter);
+
+            recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+        } else {
+            //没有视频
+            tvNomedia.setVisibility(View.VISIBLE);
+        }
+
+        progressbar.setVisibility(View.GONE);
+
+    }
+
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
--- app/src/main/java/com/atguigu/liangcang/elite/fragment/RecommendFragment.java
+++ app/src/main/java/com/atguigu/liangcang/elite/fragment/RecommendFragment.java
@@ -12,6 +12,8 @@ import com.atguigu.liangcang.base.BaseFragment;
 import com.atguigu.liangcang.elite.adapter.RecommendAdapter;
 import com.atguigu.liangcang.elite.bean.RecommendBean;
 import com.atguigu.liangcang.utils.UIUtils;
+import com.cjj.MaterialRefreshLayout;
+import com.cjj.MaterialRefreshListener;
 import com.google.gson.Gson;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
@@ -32,15 +34,37 @@ public class RecommendFragment extends BaseFragment {
     ProgressBar progressbar;
     @Bind(R.id.tv_nomedia)
     TextView tvNomedia;
+    @Bind(R.id.refresh)
+    MaterialRefreshLayout refresh;
 
     private String url = "http://s.budejie.com/topic/list/jingxuan/1/budejie-android-6.6.3/0-20.json";
 
     private RecommendAdapter adapter;
 
+
     @Override
     public View initView() {
         View view = View.inflate(context, R.layout.fragment_recommend, null);
         ButterKnife.bind(this, view);
+
+        refresh.setMaterialRefreshListener(new MaterialRefreshListener() {
+            //下拉刷新
+            @Override
+            public void onRefresh(MaterialRefreshLayout materialRefreshLayout) {
+               netWorking();
+            }
+
+//            //加载更多-上拉刷新   不做啦
+            @Override
+            public void onRefreshLoadMore(MaterialRefreshLayout materialRefreshLayout) {
+                super.onRefreshLoadMore(materialRefreshLayout);
+
+                //结束上拉刷新
+                refresh.finishRefreshLoadMore();
+            }
+        });
+
+
         return view;
     }
 
@@ -70,6 +94,8 @@ public class RecommendFragment extends BaseFragment {
                             @Override
                             public void onResponse(String response, int id) {
                                 Log.e("TAG", "RecommendFragment联网成功==" + response);
+                                //下拉刷新结束
+                                refresh.finishRefresh();
                                 //解析数据
                                 parseData(response);
                             }
@@ -85,7 +111,7 @@ public class RecommendFragment extends BaseFragment {
 
         Log.e("TAG", "name ==" + recommendBean.getList().get(0).getTags().get(0).getName());
 
-        if(recommendBean.getList() != null && recommendBean.getList().size() >0){
+        if (recommendBean.getList() != null && recommendBean.getList().size() > 0) {
             //有视频
             tvNomedia.setVisibility(View.GONE);
             //设置适配器
@@ -93,7 +119,7 @@ public class RecommendFragment extends BaseFragment {
             recyclerView.setAdapter(adapter);
 
             recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
-        }else{
+        } else {
             //没有视频
             tvNomedia.setVisibility(View.VISIBLE);
         }
@@ -101,7 +127,6 @@ public class RecommendFragment extends BaseFragment {
         progressbar.setVisibility(View.GONE);
 
     }
-
 
 
     @Override
